c0699a0d754564d8e07841b4cf3e95781cf07f0c
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2023-12-15 14:27:27 -0500

    terminando codigo de logeo

--- src/main/java/com/spring/jwt/service/AuthService.java
+++ src/main/java/com/spring/jwt/service/AuthService.java
@@ -8,6 +8,9 @@ import com.spring.jwt.repository.UserRespository;
 import com.spring.jwt.user.Role;
 import com.spring.jwt.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +21,15 @@ public class AuthService {
     private final UserRespository userRespository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authManager;
 
     public AuthResponse login(LoginRequest request) {
-        return null;
+        authManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRespository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
     }
 
     public AuthResponse register(RegisterRequest request) {
