cbc550efc855eb00fc07b82e3ee09d921bdd6624
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2023-12-15 13:33:20 -0500

    implementación de clases para crear el token jwt

---
+++ src/main/java/com/spring/jwt/config/ApplicationConfig.java
@@ -0,0 +1,4 @@
+package com.spring.jwt.config;
+
+public class ApplicationConfig {
+}
--- src/main/java/com/spring/jwt/service/AuthService.java
+++ src/main/java/com/spring/jwt/service/AuthService.java
@@ -3,10 +3,12 @@ package com.spring.jwt.service;
 import com.spring.jwt.entity.AuthResponse;
 import com.spring.jwt.entity.LoginRequest;
 import com.spring.jwt.entity.RegisterRequest;
+import com.spring.jwt.jwt.JwtService;
 import com.spring.jwt.repository.UserRespository;
 import com.spring.jwt.user.Role;
 import com.spring.jwt.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +16,8 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRespository userRespository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
 
     public AuthResponse login(LoginRequest request) {
         return null;
@@ -22,7 +26,7 @@ public class AuthService {
     public AuthResponse register(RegisterRequest request) {
         User user = User.builder()
                 .username(request.getUsername())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .lastname(request.getLastname())
                 .firstname(request.getFirstname())
                 .country(request.getCountry())
@@ -30,8 +34,9 @@ public class AuthService {
                 .build();
 
         userRespository.save(user);
+
         return AuthResponse.builder()
-                .token()
+                .token(jwtService.getToken(user))
                 .build();
     }
 
